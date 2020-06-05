6065047f74c8ee2353829c0989449b1d2e937f86
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-06-04 21:07:48 -0500

    modify server.js file

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-
-app.set("port", PORT);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("client/build"));
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, public))
+})
+
 app.get("/api", function(req, res) {
     res.json({message: "The button worked!"});
 })
 
-app.listen(app.get("port"), () => {
+app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
 })

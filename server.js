b2477b05e9defa2300a3b52e8fe3b1d19bfb409d
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-06-04 21:55:10 -0500

    update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("client/build"));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "build"))
+    res.sendFile(path.join(__dirname, "client/build"))
 })
 
 app.get("/api", function(req, res) {
